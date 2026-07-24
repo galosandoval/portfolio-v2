b746355ddfe6d44e9016789d302542aebb259979
Galo Sandoval <galosan92@gmail.com>
2026-07-23 22:18:38 -0600

    Update Card component styles to adjust position of decorative elements and enhance visual feedback on hover and focus states.

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,9 @@ export function Card({
     <li className="group relative grid rounded-lg transition-all duration-300 sm:grid-cols-8 sm:gap-8 md:gap-2 lg:hover:!opacity-100 lg:focus-within:!opacity-100 lg:group-hover/list:opacity-50 lg:group-focus-within/list:opacity-50">
       <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-background/50 lg:group-focus-within:bg-background/50"></div>
 
-      <div className="absolute -inset-x-4 bottom-0 z-0 hidden h-0.5 origin-center scale-x-0 bg-primary transition-transform duration-300 ease-out motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:scale-x-100 lg:group-focus-within:scale-x-100"></div>
+      <div className="absolute -inset-x-4 -top-4 z-0 hidden h-0.5 origin-center scale-x-0 bg-primary transition-transform duration-300 ease-out motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:scale-x-100 lg:group-focus-within:scale-x-100"></div>
+
+      <div className="absolute -inset-x-4 -bottom-4 z-0 hidden h-0.5 origin-center scale-x-0 bg-primary transition-transform duration-300 ease-out motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:scale-x-100 lg:group-focus-within:scale-x-100"></div>
 
       <div className="z-10 flex flex-col gap-2 sm:order-2 sm:col-span-6">
         {children}
